ea567e0a00eb1935640c73dd548e46ece22400bf
Nick zambo <nzambo@gmail.com>
2019-08-13 10:38:12 -0400

    final version working for TCL 43" montitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,98 @@
 #include <Arduino.h>
 #include <IRremote.h>
+//#include <EEPROM.h>
+#include <EEPROMex.h>
 
-const int RECV_PIN = 12;
-IRrecv irrecv(RECV_PIN);
+
 IRsend irsend;
-decode_results results;
 
-const int sendMode = 1;
+char incomingChar; 
+long startupDelay = 0;
+String startupDelaySerial; //i smell a buffer overflow waiting to happen
+int serialReadCounter = 0;
 
-void setup() {
 
-  Serial.begin(19200);
-  Serial.println("IR Widget starting");
-  
-  if(sendMode ==1 ) {
-    irsend.sendNEC(1474553879, 32);
-  } else {
-    irrecv.enableIRIn();
-  }
+
+
+String lts(long l) {
+  char buffer[10];
+  String retVal = ltoa(l, buffer, 10);
+  return retVal;
+}
+
+void logToSerial(String s) {
+  Serial.println(lts(millis()) + ": " + s);
 }
 
 
+void sendPowerCommand() {
+  irsend.sendNEC(1474553879, 32);
+}
 
-void printCodeStuff(decode_results *results) {
-  Serial.print("decode type: " );
-  Serial.println(results->decode_type);
+void setup() {
+  Serial.begin(19200);
+  EEPROM.setMemPool(0,EEPROMSizeNano);
+  EEPROM.setMaxAllowedWrites(100);
+  
+  logToSerial("IR Widget starting");
+  
+  startupDelay = EEPROM.readLong(0);
 
-  switch(results->decode_type) {
-    case NEC: Serial.println("NEC"); break;
-    case SONY: Serial.println("SONY"); break;
-    case PANASONIC: Serial.println("PANASONIC"); break;
-    case JVC: Serial.println("JVC"); break;
-    default:
-      Serial.println("Unknown type");
+  logToSerial("Startup delay time = " + lts(startupDelay));
+  
+  //Just sleep for the amount of time set in eeprom
+  delay(startupDelay);
 
-  }
+  //This code is specifically the power cycle code for the NEC.  For other TVs
+  //you would need to replace this.  If someone wants to be a pal and look up
+  //the most popular codes and make that a feature, have at it.
+  logToSerial("Delay over, sending IR Code for power");
+  sendPowerCommand();
+}
 
-  Serial.print("value: " );
-  Serial.println(results->value, HEX);
-  Serial.println(results->value);
-
-  Serial.print("bits: " );
-  Serial.println(results->bits);
-
- 
+void setupdebug() {
+  Serial.begin(19200);
+  EEPROM.writeLong(0,30000);
+  long tep = EEPROM.readLong(0);
+  Serial.println(tep);
 }
 
 
 void loop() {
-  if(sendMode != 1) {
-    if(irrecv.decode(&results)) {
-      Serial.println("got code");
-      printCodeStuff(&results);
-      irrecv.resume();
+  //while(Serial.available()) {
+    if (Serial.available() > 0) {
+      incomingChar = Serial.read();
+
+      //use Dxxxxx through serial to tell the chip to store that xxxxx value as the startup time
+      if(incomingChar == 'D') {
+        logToSerial("Received command to set startup delay time");
+        serialReadCounter = 0;
+        startupDelaySerial = "";
+
+        //watch for EOL to know we're done collecting the digits for the delay value (max 99,999)
+        while(Serial.available() > 0 && incomingChar != '\n' && incomingChar != '\r' && serialReadCounter++ < 5) { 
+          logToSerial("Read another character from serial");
+          incomingChar = Serial.read(); 
+          if(isDigit(incomingChar)) {
+            logToSerial("added a digit");
+            startupDelaySerial += incomingChar;
+          }
+        }
+        logToSerial("Received the following string from serial: " + startupDelaySerial);
+        //we've gotten our digits
+        startupDelay = atol(startupDelaySerial.c_str());
+        
+        logToSerial("Received a command to set the delay startup to " + startupDelaySerial + " millis");
+        EEPROM.writeLong(0,startupDelay);
+        logToSerial("Successfully wrote to EEPROM"); //that's a bit presumptuous. i know.
+
+      }
+
+      if(incomingChar == 'P') {
+        logToSerial("Received command to send power signal");
+        sendPowerCommand();
+      }
+
     }
-  }
+  //}  
 }
